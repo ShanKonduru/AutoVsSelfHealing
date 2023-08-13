7c432ea25d7e7c1e42883af55480bed9584b335a
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-08-13 18:21:43 +0530

    almost final version of ux tests

--- uxtest/uxtest.py
+++ uxtest/uxtest.py
@@ -1,4 +1,9 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
 import time
 import unittest
 
@@ -14,35 +19,38 @@ class TestUI(unittest.TestCase):
         # Create Chrome WebDriver instance using options
         # self.driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
         # self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.Chrome()
+        
+        # Create a WebDriver instance (example using Chrome)
+        self.driver = Chrome()
 
     def test_addition(self):
         self.driver.get("http://localhost:8888/ux")  # Change the URL as needed
-
-        num1_input = self.driver.find_element_by_id("num1")
-        num2_input = self.driver.find_element_by_id("num2")
-        add_button = self.driver.find_element_by_id("addBtn")
-
+        time.sleep(2)
+        num1_input = self.driver.find_element(By.ID, "num1")
+        num2_input = self.driver.find_element(By.ID, "num2")
+        add_button = self.driver.find_element(By.ID, "addBtn")
+        time.sleep(2)
         num1_input.send_keys("5")
         num2_input.send_keys("3")
         add_button.click()
-
-        result_label = self.driver.find_element_by_id("resultLabel")
+        time.sleep(2)
+        result_label = self.driver.find_element(By.ID,"resultLabel")
         result_text = result_label.text
         self.assertEqual(result_text, "Result: 8")
 
     def test_subtraction(self):
         self.driver.get("http://localhost:8888/ux")  # Change the URL as needed
-
-        num1_input = self.driver.find_element_by_id("num1")
-        num2_input = self.driver.find_element_by_id("num2")
-        add_button = self.driver.find_element_by_id("subtractBtn")
-
+        time.sleep(2)
+        num1_input = self.driver.find_element(By.ID, "num1")
+        num2_input = self.driver.find_element(By.ID, "num2")
+        add_button = self.driver.find_element(By.ID, "subtractBtn")
+        time.sleep(2)
         num1_input.send_keys("5")
         num2_input.send_keys("3")
         add_button.click()
-
-        result_label = self.driver.find_element_by_id("resultLabel")
+        time.sleep(2)
+        result_label = self.driver.find_element(By.ID, "resultLabel")
         result_text = result_label.text
         self.assertEqual(result_text, "Result: 2")
 
