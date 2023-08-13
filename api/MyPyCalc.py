ebf1ef68b1407dbd3d5e824e5d675bc79b22ee74
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-08-13 11:28:35 +0530

    working copy

--- api/MyPyCalc.py
+++ api/MyPyCalc.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:8888"}})
 
 @app.route('/add', methods=['POST'])
 def add():
--- ux/app.py
+++ ux/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/ux', methods=['GET'])
 def serve_html():
