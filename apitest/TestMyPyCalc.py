298977469f013653f64e7085d6e69e017cb6db4d
Shan Konduru <ShanKonduru@users.noreply.github.com>
2023-08-13 10:26:26 +0530

    aded ux module

--- apitest/TestMyPyCalc.py
+++ apitest/TestMyPyCalc.py
@@ -9,21 +9,21 @@ class MyPyCalcTest(unittest.TestCase):
 
     def test_add(self):
         data = {'num1': 5, 'num2': 3}
-        response = self.app.post('/add', data=json.dumps(data), content_type='application/json')
+        response = self.app.post('/api/add', data=json.dumps(data), content_type='application/json')
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(result['result'], 8)
 
     def test_subtract(self):
         data = {'num1': 10, 'num2': 4}
-        response = self.app.post('/subtract', data=json.dumps(data), content_type='application/json')
+        response = self.app.post('/api/subtract', data=json.dumps(data), content_type='application/json')
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(result['result'], 6)
 
     def test_missing_data(self):
         data = {'num1': 5}  # Missing 'num2'
-        response = self.app.post('/add', data=json.dumps(data), content_type='application/json')
+        response = self.app.post('/api/add', data=json.dumps(data), content_type='application/json')
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 400)
         self.assertEqual(result['error'], 'Missing data')
