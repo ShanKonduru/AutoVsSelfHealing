a38b0ba14c71b0b54d1f3327d7588b16a944af89
ShanKonduru <ShanKonduru@gmail.com>
2023-08-20 18:53:34 +0530

    updated scale commands

--- 010-stackscale.bat
+++ 010-stackscale.bat
@@ -1,10 +1,19 @@
 @echo off
 
+echo pull ux image from docker hub
 docker pull shankonduru/autovsselfhealing-ux
+
+echo chnage ux image scale
 docker service scale shankonduru/autovsselfhealing-ux=2 
 
+echo pull db image from docker hub
 docker pull shankonduru/autovsselfhealing-db
+
+echo chnage db image scale
 docker service scale shankonduru/autovsselfhealing-db=2 
 
+echo pull api image from docker hub
 docker pull shankonduru/autovsselfhealing-api
+
+echo chnage api image scale
 docker service scale shankonduru/autovsselfhealing-api=2
