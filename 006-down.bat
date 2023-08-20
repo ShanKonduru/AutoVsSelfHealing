9d2fdf8edb5456e10393df98c00630edb552e7ec
ShanKonduru <ShanKonduru@gmail.com>
2023-08-20 12:51:22 +0530

    add only one replica

--- 006-down.bat
+++ 006-down.bat
@@ -3,4 +3,11 @@ REM docker-compose down -v
 REM docker image prune --force
 
 docker-compose down -v 
+
 docker image prune --force
+
+echo Removing all Docker images...
+for /f %%i in ('docker images -aq') do (
+    docker rmi -f %%i
+)
+echo All Docker images removed.
